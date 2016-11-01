7e8a29976abe80447e44d8c3740f8e0672fda953
Steve Kinney <hello@stevekinney.net>
2016-11-01 11:17:58 -0600

    Add ability to format tweets and first pass at JSON API compliance

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 'use strict';
-const Twitter = require('node-twitter-api');
 const express = require('express');
 const app = express();
 
-const twitter = new Twitter(require('./env.json'));
+const getTweetsForHashTag = require('./lib/get-tweets-for-hashtag');
+const formatTweet = require('./lib/format-tweet');
 
 app.set('port', process.env.PORT || 3001);
 
@@ -11,18 +11,9 @@ app.listen(app.get('port'), () => {
  console.log(`listening on ${app.get('port')}`);
 });
 
-const getTweetsForHashTag = (hashtag, numberOfTweets = 100, callback = () => {}) => {
- twitter.search(
-   {"q": hashtag, "count": numberOfTweets},
-   twitter.accessToken,
-   twitter.accessTokenSecret,
-   callback
- );
-};
-
-app.get('/api/v1/hashtags/:hashtag', function(req, res) {
-  getTweetsForHashTag(req.params.hashtag, req.query.count, (error, data, response) => {
+app.get('/api/v1/tweets', function(req, res) {
+  getTweetsForHashTag(req.query.hashtag, req.query.count, (error, data, response) => {
     if (error) return res.send({ error });
-    res.send({ data: data.statuses });
+    res.send({ data: data.statuses.map(formatTweet) });
   });
 });
